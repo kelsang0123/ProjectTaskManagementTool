9438359797818a7ceb630770850e50c38546383c
kelsang <kelsangsp2005@gmail.com>
2026-07-10 23:15:05 +0200

    intial commits on app backend for sprint 1

---
+++ AppServer/Entities/Project.cs
@@ -0,0 +1,10 @@
+namespace Entities;
+
+public class Project
+{
+    public int Id{get; set;}
+    public required string Title{get; set;}
+    public required string Description{get; set;}
+    public required string Status{get; set;}
+    public DateTime CreatedDateTime{get; set;}
+}
---
+++ AppServer/RepositoryContracts/IProjectRepository.cs
@@ -0,0 +1,12 @@
+using Entities;
+
+namespace RepositoryContracts;
+
+public interface IProjectRepository
+{
+  Task<Project> AddAsync(Project project);
+  Task UpdateAsync(Project project);
+  Task DeleteAsync(int id);
+  Task<Project> GetSingleAsync(int id);
+  IQueryable<Project> GetManyAsync();
+}
