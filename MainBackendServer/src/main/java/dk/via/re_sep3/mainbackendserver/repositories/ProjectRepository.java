027979365305adc16de5e907a6fe8349eced881a
kelsang <kelsangsp2005@gmail.com>
2026-07-20 12:00:18 +0200

    sprint1 repository commits

--- MainBackendServer/src/main/java/dk/via/re_sep3/mainbackendserver/repositories/ProjectRepository.java
+++ MainBackendServer/src/main/java/dk/via/re_sep3/mainbackendserver/repositories/ProjectRepository.java
@@ -1,15 +1,11 @@
 package dk.via.re_sep3.mainbackendserver.repositories;
 
-import com.google.protobuf.Timestamp;
-import com.google.type.DateTime;
 import dk.via.re_sep3.mainbackendserver.domain.Project;
 
 import java.sql.SQLException;
-import java.time.Instant;
 import java.util.List;
 
 public interface ProjectRepository
-{
-  List<Project> getProjects() throws SQLException;
+{  List<Project> getProjects() throws SQLException;
   Project registerProject(String title, String description, int creatorId);
 }
